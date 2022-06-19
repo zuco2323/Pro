927e8a9a07dc99a748b0dac4202ac1edb1c63808
zuco2323 <juancholegna87@gmail.com>
2022-06-19 18:16:08 -0300

    xd

--- Pro/src/app/app.module.ts
+++ Pro/src/app/app.module.ts
@@ -12,6 +12,8 @@ import { ExperienciaComponent } from './components/experiencia/experiencia.compo
 import { EducacionComponent } from './components/educacion/educacion.component';
 import { NgCircleProgressModule } from 'ng-circle-progress';
 import { HysComponent } from './components/hys/hys.component';
+import { ProyectoComponent } from './components/proyecto/proyecto.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 
 @NgModule({
@@ -25,6 +27,8 @@ import { HysComponent } from './components/hys/hys.component';
     ExperienciaComponent,
     EducacionComponent,
     HysComponent,
+    ProyectoComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
